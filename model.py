08a5c3bdb2b6cc45f4a1e91e4008a85e22080547
MICHAEL KOFI ARMAH <85971733+mk-armah@users.noreply.github.com>
2022-04-15 11:09:52 +0000

    Created using Colaboratory

---
+++ model.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "faceid.py",
+      "provenance": [],
+      "authorship_tag": "ABX9TyPyOarXhj3eeyCJm8YuS+Oo",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/mk-armah/BECE-Grade-Analysis/blob/master/model.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "!python model.py"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "73gp_0mv6NAQ",
+        "outputId": "a74c100a-2efe-4e08-d484-6065fdbdb382"
+      },
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hello world\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
